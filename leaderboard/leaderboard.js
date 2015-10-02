129a49964de9c611344fafeb3a666c75d1c3f085
Kasey McCormick <mccormick.kasey@gmail.com>
2015-10-02 12:30:35 -0500

    able to retreive players and scores

--- leaderboard/leaderboard.js
+++ leaderboard/leaderboard.js
@@ -1,23 +1,17 @@
-if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
-
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
+PlayersList = new Mongo.Collection('players');
+if(Meteor.isClient){
+  Template.leaderboard.helpers({
+  'player': function(){
+  return PlayersList.find()
+  },
+  'otherHelperFunction': function(){
+  return "Some other function"
+  }
   });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
+  
+console.log("Hello client");
 }
 
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
+if(Meteor.isServer){
+console.log("Hello server");
 }
