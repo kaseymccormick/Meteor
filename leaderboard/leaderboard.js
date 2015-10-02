3ba0bbfb0710285eeb1caf3f2d7f5c61270b1996
Kasey McCormick <mccormick.kasey@gmail.com>
2015-10-02 15:15:51 -0500

    player added and removed

--- leaderboard/leaderboard.js
+++ leaderboard/leaderboard.js
@@ -1,17 +1,64 @@
 PlayersList = new Mongo.Collection('players');
 if(Meteor.isClient){
   Template.leaderboard.helpers({
-  'player': function(){
-  return PlayersList.find()
-  },
-  'otherHelperFunction': function(){
-  return "Some other function"
-  }
+    'player': function(){
+      return PlayersList.find({}, {sort: {score: -1, name: 1} })
+    },
+    'selectedClass': function(){
+      // return "selected"
+      var playerId = this._id;
+      var selectedPlayer = Session.get('selectedPlayer');
+      if(playerId == selectedPlayer){
+        return "selected"
+      }
+    },
+    'showSelectedPlayer': function(){
+      var selectedPlayer = Session.get('selectedPlayer');
+      return PlayersList.findOne(selectedPlayer)
+    }
   });
   
-console.log("Hello client");
+  
+  Template.leaderboard.events({
+    'click .player': function(){
+      var playerId = this._id;
+      Session.set('selectedPlayer', playerId);
+    }, //comma seperates the events and is needed
+    'click .increment': function(){
+      var selectedPlayer = Session.get('selectedPlayer');
+      PlayersList.update(selectedPlayer, {$inc: {score:5} });
+    },
+    'click .decrement': function(){
+      var selectedPlayer = Session.get('selectedPlayer');
+      PlayersList.update(selectedPlayer, {$inc: {score: -5} });
+    },
+    'click .remove': function(){
+      var selectedPlayer = Session.get('selectedPlayer');
+      PlayersList.remove(selectedPlayer);
+    }
+  });
+  
+  
+  Template.addPlayerForm.events({
+    'submit form': function(event){
+      event.preventDefault();
+      var playerNameVar = event.target.playerName.value;
+      console.log(playerNameVar);
+    },
+    'submit form': function(event){
+      event.preventDefault();
+      var playerNameVar = event.target.playerName.value;
+      PlayersList.insert({
+        name: playerNameVar,
+        score: 0
+      });
+    }
+  });
+  
+  
+  console.log("Hello client");
 }
 
 if(Meteor.isServer){
-console.log("Hello server");
+  console.log("Hello server");
 }
