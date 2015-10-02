e6ee4e2f51a16d46d40ae91077cb4f5a67451c50
Kasey McCormick <mccormick.kasey@gmail.com>
2015-10-02 16:39:13 -0500

    file structure messing about

---
+++ leaderboard/client/client.js
@@ -0,0 +1,53 @@
+if(Meteor.isClient){
+  Template.leaderboard.helpers({
+    'player': function(){
+      var currentUserId = Meteor.userId();
+      return PlayersList.find({}, {sort: {score: -1, name: 1}});
+    },
+    'selectedClass': function(){
+      // return "selected"
+      var playerId = this._id;
+      var selectedPlayer = Session.get('selectedPlayer');
+      if(playerId == selectedPlayer){
+        return "selected"
+      }
+    },
+    'showSelectedPlayer': function(){
+      var selectedPlayer = Session.get('selectedPlayer');
+      return PlayersList.findOne(selectedPlayer)
+    }
+  });
+  
+  
+  Template.leaderboard.events({
+    'click .player': function(){
+      var playerId = this._id;
+      Session.set('selectedPlayer', playerId);
+    }, //comma seperates the events and is needed
+    'click .increment': function(){
+      var selectedPlayer = Session.get('selectedPlayer');
+      Meteor.call('modifyPlayerScore', selectedPlayer, 5);
+    },
+    'click .decrement': function(){
+      var selectedPlayer = Session.get('selectedPlayer');
+      Meteor.call('modifyPlayerScore', selectedPlayer, -5);
+    },
+    'click .remove': function(){
+      var selectedPlayer = Session.get('selectedPlayer');
+      Meteor.call('removePlayerData', selectedPlayer);
+    }
+  });
+  
+  
+  Template.addPlayerForm.events({
+    
+    'submit form': function(event){
+      event.preventDefault();
+      var playerNameVar = event.target.playerName.value;
+      Meteor.call('insertPlayerData', playerNameVar);
+    }
+  });
+  
+  
+  Meteor.subscribe('thePlayers');
+}
