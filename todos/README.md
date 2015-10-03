05c1623ce0f43254251ceb70ecbb0dbf24886d31
Kasey McCormick <mccormick.kasey@gmail.com>
2015-10-02 20:24:42 -0500

    changed to meteor tutorial of todo app

---
+++ todos/README.md
@@ -0,0 +1,2 @@
+started from http://meteortips.com/second-meteor-tutorial/methods-part-1/
+then moved on to https://www.meteor.com/tutorials/blaze/creating-an-app
--- todos/todos.js
+++ todos/todos.js
@@ -1,39 +1,10 @@
-Todos = new Meteor.Collection('todos');
-if(Meteor.isClient){
-  
-  Template.todos.helpers({
-    'todo' : function(){
-      return Todos.find({},{sort:{createdAt:-1}});
+Tasks = new Mongo.Collection("tasks");
+ 
+if (Meteor.isClient) {
+  // This code only runs on the client
+  Template.body.helpers({
+    tasks: function () {
+      return Tasks.find({});
     }
   });
-  
-  Template.addTodo.events({
-    
-    'submit form': function(event){
-      event.preventDefault();
-      var todoName = event.target.todoName.value;
-      Todos.insert({
-        name: todoNmae,
-        completed: false,
-        createdAt: new Date()
-      });
-    }
-  });
-  
-  Template.todoItem.events({
-    'click .delete-todo' : function(event){
-      event.preventDefault();
-      var documentId = this._id;
-      var confirm = window.confirm("delete this task");
-      if(confirm){
-        Todos.remove({_id: documentId});
-      }
-    }
-  });
-  
 }
-
-if(Meteor.isServer){
-  // server code goes here
-}
-
